6f5419db318d520d6fdaa07c84de706aa6524870
VidushiShekhar <shekharvidushi@gmail.com>
2022-01-24 17:54:08 +0530

    change sqlite to postgres

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ def admin_only(f):
     return decorated_function
 # #CONNECT TO DB
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
